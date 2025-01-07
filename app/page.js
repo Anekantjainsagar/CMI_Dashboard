fd55fc4c66bd767639bb03e790d5bb635592dba4
Anekantjainsagar <anekantjainsagar@gmail.com>
2025-01-07 18:06:06 +0530

    Updated login page

--- app/page.js
+++ app/page.js
@@ -7,6 +7,9 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { BACKEND_URI } from "./Utils/urls";
 import { setCookie } from "cookies-next";
 import Context from "./Context/Context";
+import Image from "next/image";
+import { CiLogin } from "react-icons/ci";
+import { MdEmail, MdLock } from "react-icons/md";
 
 const App = () => {
   const { getMainData } = useContext(Context);
@@ -14,66 +17,88 @@ const App = () => {
   const history = useRouter();
 
   return (
-    <div className="flex items-center justify-center h-[100vh]">
-      <div className="w-[25vw] border px-5 py-3 border-aquaGreen rounded-lg flex flex-col">
-        <h1 className="text-3xl text-center text-aquaGreen font-semibold">
-          Login Now
-        </h1>
-        <div className="w-full">
-          <label htmlFor="email" className="block mb-2 text-gray-900">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            value={user?.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-            className="w-full py-2 px-4 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none bg-white text-gray-900 placeholder-gray-500 text-lg"
-            placeholder="Enter your email"
+    <div className="flex items-center relative justify-center h-[100vh]">
+      {/* <div className="flex items-center absolute top-5 left-5">
+        <div className="flex items-center cursor-pointer">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={1000}
+            height={1000}
+            className="w-[3vw] cursor-pointer"
           />
+          <h2 className="text-2xl font-semibold ml-3">Invoice Summary</h2>
         </div>
-        <PasswordInput user={user} setUser={setUser} />
-        <button
-          onClick={() => {
-            if (user?.email && user?.password) {
-              axios
-                .post(
-                  `${BACKEND_URI}/auth/login`,
-                  {
-                    username: user?.email,
-                    password: user?.password,
-                  },
-                  {
-                    headers: {
-                      "Content-Type": "application/x-www-form-urlencoded",
+      </div> */}
+      <div className="w-[24vw] shadow-md shadow-aquaGreen px-7 py-8 rounded-3xl flex flex-col items-center justify-between">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={1000}
+          height={1000}
+          className="w-[6vw] cursor-pointer my-6"
+        />
+        <div className="w-full"> 
+          <h1 className="text-4xl text-center text-black font-[700]">
+            Login
+          </h1>
+          {/* <p className="text-gray-500 text-center w-10/12 mx-auto text-lg my-2">
+            Enter your email address & password to login into the CMI Portal
+          </p> */}
+          <div className="w-full mt-6 relative">
+            <MdEmail className="absolute left-4 text-gray-400 top-1/2 -translate-y-1/2 text-2xl" />
+            <input
+              id="email"
+              type="email"
+              value={user?.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              className="w-full py-3 pl-14 pr-4 bg-gray-200/50 rounded-lg text-lg text-gray-700/70 placeholder-gray-500 outline-none"
+              placeholder="Enter your email"
+            />
+          </div>
+          <PasswordInput user={user} setUser={setUser} />
+          <button
+            onClick={() => {
+              if (user?.email && user?.password) {
+                axios
+                  .post(
+                    `${BACKEND_URI}/auth/login`,
+                    {
+                      username: user?.email,
+                      password: user?.password,
                     },
-                  }
-                )
-                .then((res) => {
-                  console.log(res.data);
-                  if (res.status == 200) {
-                    if (res.data.access_token) {
-                      setCookie("token", res.data.access_token);
-                      toast.success("Login Successfully");
-                      getMainData();
-                      history.push("/dashboard");
-                    } else {
-                      console.log(res.data[Object.keys(res.data)[0]]);
-                      toast.error(res.data[Object.keys(res.data)[0]]);
+                    {
+                      headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                      },
                     }
-                  }
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
-            } else {
-              toast.error("Please fill all the details");
-            }
-          }}
-          className="bg-aquaGreen transition-all px-10 py-1.5 rounded-lg mt-5 text-xl text-white"
-        >
-          Submit
-        </button>
+                  )
+                  .then((res) => {
+                    console.log(res.data);
+                    if (res.status == 200) {
+                      if (res.data.access_token) {
+                        setCookie("token", res.data.access_token);
+                        toast.success("Login Successfully");
+                        getMainData();
+                        history.push("/dashboard");
+                      } else {
+                        console.log(res.data[Object.keys(res.data)[0]]);
+                        toast.error(res.data[Object.keys(res.data)[0]]);
+                      }
+                    }
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              } else {
+                toast.error("Please fill all the details");
+              }
+            }}
+            className="bg-aquaGreen w-full transition-all px-10 py-2 rounded-lg mt-5 font-semibold text-xl text-white"
+          >
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -82,28 +107,25 @@ const App = () => {
 const PasswordInput = ({ user, setUser }) => {
   const [isVisible, setIsVisible] = useState(false);
 
-  // Toggle password visibility
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   };
 
   return (
     <div className="w-full mt-4">
-      <label htmlFor="password" className="block mb-2 text-gray-900">
-        Password
-      </label>
       <div className="relative">
+        <MdLock className="absolute left-4 text-gray-400 top-1/2 -translate-y-1/2 text-2xl" />
         <input
           id="password"
           value={user?.password}
           onChange={(e) => setUser({ ...user, password: e.target.value })}
           type={isVisible ? "text" : "password"}
-          className="w-full py-2 pl-4 pr-10 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none bg-white text-gray-900 placeholder-gray-500 text-lg"
+          className="w-full py-3 px-14 bg-gray-200/50 text-lg rounded-lg text-gray-700/70 placeholder-gray-500 outline-none"
           placeholder="Enter your password"
         />
         <button
           type="button"
-          className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-600 dark:text-gray-400"
+          className="absolute inset-y-0 right-0 text-xl flex items-center pr-4 text-gray-400"
           onClick={toggleVisibility}
         >
           {isVisible ? <FaEyeSlash /> : <FaEye />}
